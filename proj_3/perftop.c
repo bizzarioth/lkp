28a05afc422ad0e667121a2574a30e909c256a58
bizzarioth <wittymukund19@gmail.com>
2021-10-04 00:16:58 -0400

    update 1.2

--- proj_3/perftop.c
+++ proj_3/perftop.c
@@ -5,9 +5,49 @@
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 
+#include <linux/kprobes.h>
+
+#define MAX_SYMBOL_LEN  64
+static char symbol[MAX_SYMBOL_LEN] = "proc_opener";
+
+/* For each probe you need to allocate a kprobe structure */
+static struct kprobe kproc_open = {
+  .symbol_name  = symbol,
+};
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("[Mukund Agarwal]");
 MODULE_DESCRIPTION("Project - 3");
+
+/* kprobe pre_handler: called just before the probed instruction is executed */
+static int __kprobes handler_pre(struct kprobe *p, struct pt_regs *regs)
+{
+#ifdef CONFIG_X86
+  pr_info("<%s> p->addr = 0x%p, ip = %lx, flags = 0x%lx\n",
+    p->symbol_name, p->addr, regs->ip, regs->flags);
+#endif
+#ifdef CONFIG_ARM64
+  pr_info("<%s> p->addr = 0x%p, pc = 0x%lx, pstate = 0x%lx\n",
+    p->symbol_name, p->addr, (long)regs->pc, (long)regs->pstate);
+#endif
+  /* A dump_stack() here will give a stack backtrace */
+  return 0;
+}
+
+/* kprobe post_handler: called after the probed instruction is executed */
+static void __kprobes handler_post(struct kprobe *p, struct pt_regs *regs,
+  unsigned long flags)
+{
+#ifdef CONFIG_X86
+  pr_info("<%s> p->addr = 0x%p, flags = 0x%lx\n",
+    p->symbol_name, p->addr, regs->flags);
+#endif
+#ifdef CONFIG_ARM64
+  pr_info("<%s> p->addr = 0x%p, pstate = 0x%lx\n",
+    p->symbol_name, p->addr, (long)regs->pstate);
+#endif
+}
+
 
 static int proc_show(struct seq_file *m, void *v){
   printk(KERN_INFO "Hello world kmesg!\n");
@@ -40,17 +80,35 @@ static const struct proc_ops myops =
   .proc_release = single_release,
 };
 
+static int kprobe_init(void){
+  int ret;
+  kproc_open.pre_handler = handler_pre;
+  kproc_open.post_handler = handler_post;
+
+  ret = register_kprobe(&kproc_open);
+  if (ret < 0) {
+    pr_err("register_kprobe failed, returned %d\n", ret);
+    return ret;
+  }
+  pr_info("Planted kprobe at %p\n", kproc_open.addr);
+  return 0;
+}
 
 static int __init proj_init(void) {
   int err=0;
   proc_create("perftop", 0, NULL, &myops);
 
+  err+=kprobe_init();
   if(err) return err;
   return 0;
 }
 
 static void __exit proj_exit(void) {
   remove_proc_entry("perftop", NULL);
+
+  unregister_kprobe(&kproc_open);
+  pr_info("kprobe at %p unregistered\n", kproc_open.addr);
+
   return;
 }
 
